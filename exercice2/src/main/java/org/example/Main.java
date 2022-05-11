8dd63f52ec27604aaf7f74da33fa52c860b8fbea
matformation <game@le-scarabit.xyz>
2022-05-11 14:48:15 +0200

    update

--- exercice2/src/main/java/org/example/Main.java
+++ exercice2/src/main/java/org/example/Main.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Yo! Choisis ton numéro frangin: ");
-    int choix = scanner.nextInt();
-    for (int i = 0 ; i <= 10; i++){
-        int result = choix * i;
-        System.out.println(i + " * " + choix + " = " + result);
-    }
+    while(true) {
+        try {
+            System.out.print("Yo! Choisis ton numéro frangin: ");
 
+            int choix = scanner.nextInt();
+            for (int i = 0; i <= 10; i++) {
+                int result = choix * i;
+                System.out.println(i + " * " + choix + " = " + result);
+            }
+        } catch (Exception e) {
+            System.out.println("t'es serieux là?");
+            scanner.nextLine();
+        }
+    }
     }
 }
