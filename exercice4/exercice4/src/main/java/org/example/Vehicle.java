38280f89d4dc8d6d63076f1ca917e270452b9126
matformation <game@le-scarabit.xyz>
2022-05-13 16:28:20 +0200

    exercice4

--- exercice4/exercice4/src/main/java/org/example/Vehicle.java
+++ exercice4/exercice4/src/main/java/org/example/Vehicle.java
@@ -1,49 +1,91 @@
 package org.example;
 
 public abstract class Vehicle {
-        protected float currentGas;
-        protected float gasCapacity;
-        protected float gasConsumption;
-        protected int speed;
-        protected int passengerCapacity;
+    //Partie 1 getter / setter
+    protected float currentGas;
 
     public float getCurrentGas() {
         return currentGas;
-    }
-
-    public float getGasCapacity() {
-        return gasCapacity;
-    }
-
-    public float getGasConsumption() {
-        return gasConsumption;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public int getPassengerCapacity() {
-        return passengerCapacity;
     }
 
     public void setCurrentGas(float currentGas) {
         this.currentGas = currentGas;
     }
 
-    public void setGasCapacity(float gasCapacity) {
-        this.gasCapacity = gasCapacity;
-    }
+    protected int speed;
 
-    public void setGasConsumption(float gasConsumption) {
-        this.gasConsumption = gasConsumption;
+    public int getSpeed() {
+        return speed;
     }
 
     public void setSpeed(int speed) {
         this.speed = speed;
     }
 
+    protected float gasCapacity;
+
+    public float getGasCapacity() {
+        return gasCapacity;
+    }
+
+    public void setGasCapacity(float gasCapacity) {
+        this.gasCapacity = gasCapacity;
+    }
+
+    protected float gasConsumption;
+
+    public float getGasConsumption() {
+        return gasConsumption;
+    }
+
+    public void setGasConsumption(float gasConsumption) {
+        this.gasConsumption = gasConsumption;
+    }
+
+    protected int passengerCapacity;
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
     public void setPassengerCapacity(int passengerCapacity) {
         this.passengerCapacity = passengerCapacity;
+    }
+
+    //Partie 2 méthodes
+    public abstract void accelerate();
+
+    public abstract void brake();
+
+    public void refuel(int refuelQuantity) {
+
+        if (currentGas < gasCapacity) {
+            if ((refuelQuantity + currentGas) <= gasCapacity) {
+                currentGas += refuelQuantity;
+            } else {
+                float wastedGas = currentGas + refuelQuantity - gasCapacity;
+                System.out.println(String.format("Gaspille pas mon gars, tu vas devoir boire %d litres", wastedGas));
+            }
+        } else {
+            System.out.println("Ton réservoir est déjà plein gros c***n !");
+        }
+
+    }
+
+    public void drive() throws Exception {
+        //try {
+
+        float conso = (gasConsumption * speed) / 100;
+        if (currentGas - conso >= 0) {
+            accelerate();
+            currentGas -= conso;
+            System.out.println("Le véhicule a consommé : " + conso);
+        } else {
+            throw new Exception("Tu vas devoir pousser mon gars :( ");
+        }
+
+        //   } catch (Exception e) {
+        // System.out.println(e);
+        //  }
     }
 }
